3a4fd997c1f783f612bae41deeaf0c6f6f82b312
Mark Day <mday@mac.com>
2022-12-01 07:55:51 -0700

    Day 01, Part 2

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let input = std::fs::read_to_string("src/bin/day01/input.txt")
         .expect("Can't open input.txt");
 
-    let elf_totals: Vec<u32> = input.split("\n\n").map(|s|
+    let mut elf_totals: Vec<u32> = input.split("\n\n").map(|s|
         // s is the numbers for one elf
         s.split_terminator('\n').map(|n|
             // n is one number for the current elf
@@ -10,7 +10,14 @@ fn main() {
         ).sum()
     ).collect();
 
+    // Part 1
     let result1 = *elf_totals.iter().max().unwrap();
     println!("Part 1: {}", result1);
     assert_eq!(result1, 70374);
+
+    // Part 2
+    elf_totals.sort_unstable();
+    let result2: u32 = elf_totals.iter().rev().take(3).sum();
+    println!("Part 2: {}", result2);
+    assert_eq!(result2, 204610);
 }
