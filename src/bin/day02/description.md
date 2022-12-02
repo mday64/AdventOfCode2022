48d54b9cc84ef6f7ca8e789091dcd4ca6faac319
Mark Day <mday@mac.com>
2022-12-02 10:32:44 -0700

    Day 2, Part 1

--- src/bin/day02/description.md
+++ src/bin/day02/description.md
@@ -26,3 +26,5 @@ This strategy guide predicts and recommends the following:
 In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + **`6`**).
 
 **What would your total score be if everything goes exactly according to your strategy guide?**
+
+Your puzzle answer was `13268`.
--- src/bin/day02/main.rs
+++ src/bin/day02/main.rs
@@ -1,7 +1,35 @@
-use anyhow;
+use anyhow::{anyhow, bail};
 
 fn main() -> anyhow::Result<()> {
-    println!("Hello, World!");
+    let path = std::env::args().skip(1).next()
+        .unwrap_or("src/bin/day02/input.txt".into());
+    let input = std::fs::read_to_string(path)?;
+
+    // Part 1
+    let mut result1 = 0;
+    for line in input.lines() {
+        let mut letters = line.split(' ');
+        let opponent = letters.next().ok_or(anyhow!("missing first letter"))?;
+        let me = letters.next().ok_or(anyhow!("missing first letter"))?;
+
+        // Add the score for the item I chose
+        result1 += match me {
+            "X" => 1,
+            "Y" => 2,
+            "Z" => 3,
+            _ => bail!("Invalid letter for me")
+        };
+
+        // Add the score for the outcome of the match
+        result1 += match (opponent, me) {
+            ("A", "X") | ("B", "Y") | ("C", "Z") => 3,
+            ("A", "Y") | ("B", "Z") | ("C", "X") => 6,
+            ("A", "Z") | ("B", "X") | ("C", "Y") => 0,
+            (_, _) => bail!("Invalid letter combination")
+        };
+    }
+    println!("Part 1: {}", result1);
+    assert_eq!(result1, 13268);
 
     Ok(())
 }
