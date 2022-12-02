298d8402906d6111cf5986f9d87630f9d2cce8d9
Mark Day <mday@mac.com>
2022-12-01 19:32:31 -0700

    Propagates errors out to main()

--- src/bin/day01/main.rs
+++ src/bin/day01/main.rs
@@ -1,14 +1,15 @@
-fn main() {
-    let input = std::fs::read_to_string("src/bin/day01/input.txt")
-        .expect("Can't open input.txt");
+use std::{ops::{AddAssign, Add}, error::Error};
 
-    let mut elf_totals: Vec<u32> = input.split("\n\n").map(|s|
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = std::fs::read_to_string("src/bin/day01/input.txt")?;
+
+    let mut elf_totals = input.split("\n\n").map(|s| {
         // s is the numbers for one elf
-        s.split_terminator('\n').map(|n|
+        try_sum(s.split_terminator('\n').map(|n|
             // n is one number for the current elf
-            n.parse::<u32>().expect("invalid number")
-        ).sum()
-    ).collect();
+            n.parse::<u32>()
+        ))
+    }).collect::<Result<Vec<u32>,_>>()?;
 
     // Part 1
     let result1 = *elf_totals.iter().max().unwrap();
@@ -20,4 +21,16 @@ fn main() {
     let result2: u32 = elf_totals.iter().rev().take(3).sum();
     println!("Part 2: {}", result2);
     assert_eq!(result2, 204610);
+
+    Ok(())
+}
+
+fn try_sum<I,T,E>(iter: I) -> Result<T,E>
+    where T: Default + AddAssign, I: Iterator<Item=Result<T,E>>
+{
+    let mut result = T::default();
+    for x in iter {
+        result += x?;
+    }
+    Ok(result)
 }
