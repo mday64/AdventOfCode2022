b7a718fa52f8e78c35051f562689cc7ecb2007cc
Mark Day <mday@mac.com>
2022-12-10 13:15:11 -0700

    Refactor
    Function run_program() produces the list of all x-register values for the entire program.  Function part1() extracts just the values needed for part 1.

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -6,43 +6,32 @@ fn main() {
     //
     // Part 1
     //
-    // How should we solve this one?  One of the potential issues I
-    // see is that the `addx V` instruction takes two cycles, and the
-    // cycles we care about could be in the middle of an instruction.
-    // We would need to be able to detect that an interesting cycle
-    // happened, and know the value in `X` at the time of the interesting
-    // cycle.
-    //
-    // Another possibility is to create an iterator that produces the
-    // value of `X` on every cycle.  Then we skip(), step(), and take()
-    // to get the interesting values.
-    //
-    let mut interesting_cycles = [20, 60, 100, 140, 180, 220].into_iter();
-    let mut next_interesting_cycle = interesting_cycles.next().unwrap();
-    let mut cycle = 0;
-    let mut xreg = 1;
-    let mut result1 = 0;
-    for line in input.lines() {
-        if line == "noop" {
-            cycle += 1;
-        } else if line.starts_with("addx ") {
-            cycle += 2;
-        }
-
-        if cycle >= next_interesting_cycle {
-            result1 += next_interesting_cycle * xreg;
-            if let Some(n) = interesting_cycles.next() {
-                next_interesting_cycle = n;
-            } else {
-                break;
-            }
-        }
-
-        if line.starts_with("addx ") {
-            let (_, amount) = line.split_once(' ').unwrap();
-            xreg += amount.parse::<i32>().unwrap();
-        }
-    }
+    let result1 = part1(&input);
     println!("Part 1: {}", result1);
     assert_eq!(result1, 12460);
+}
+
+fn part1(input: &str) -> i32 {
+    let mut result = 0;
+    let x_values = run_program(&input);
+    for i in [20, 60, 100, 140, 180, 220] {
+        result += i as i32 * x_values[i-1];
+    }
+    result
+}
+
+fn run_program(program: &str) -> Vec<i32> {
+    let mut result = Vec::new();
+    let mut x = 1;
+    for line in program.lines() {
+        if line == "noop" {
+            result.push(x);
+        } else if line.starts_with("addx ") {
+            let (_, v) = line.split_once(' ').unwrap();
+            result.push(x);
+            result.push(x);
+            x += v.parse::<i32>().unwrap();
+        }
+    }
+    result
 }
