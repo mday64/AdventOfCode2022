7019af12ef005d55a5d2e853b8bb91130305cee7
Mark Day <mday@mac.com>
2022-12-11 12:30:18 -0700

    Convert operation to an enum

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -7,38 +7,14 @@ fn main() {
     // going to build them directly in code.
     //
     let monkeys = vec![
-        Monkey::new(
-            &[57, 58],
-            &|old| old*19,
-            7, 2, 3),
-        Monkey::new(
-            &[66, 52, 59, 79, 94, 73],
-            &|old| old+1,
-            19, 4, 6),
-        Monkey::new(
-            &[80],
-            &|old| old+6,
-            5, 7, 5),
-        Monkey::new(
-            &[82, 81, 68, 66, 71, 83, 75, 97],
-            &|old| old+5,
-            11, 5, 2),
-        Monkey::new(
-            &[55, 52, 67, 70, 69, 94, 90],
-            &|old| old*old,
-            17, 0, 3),
-        Monkey::new(
-            &[69, 85, 89, 91],
-            &|old| old+7,
-            13, 1, 7),
-        Monkey::new(
-            &[75, 53, 73, 52, 75],
-            &|old| old*7,
-            2, 0, 4),
-        Monkey::new(
-            &[94, 60, 79],
-            &|old| old+2,
-            3, 1, 6),
+        Monkey::new(&[57, 58], Operation::Multiply(19), 7, 2, 3),
+        Monkey::new(&[66, 52, 59, 79, 94, 73], Operation::Add(1), 19, 4, 6),
+        Monkey::new(&[80], Operation::Add(6), 5, 7, 5),
+        Monkey::new(&[82, 81, 68, 66, 71, 83, 75, 97], Operation::Add(5), 11, 5, 2),
+        Monkey::new(&[55, 52, 67, 70, 69, 94, 90], Operation::Square, 17, 0, 3),
+        Monkey::new(&[69, 85, 89, 91], Operation::Add(7), 13, 1, 7),
+        Monkey::new(&[75, 53, 73, 52, 75], Operation::Multiply(7), 2, 0, 4),
+        Monkey::new(&[94, 60, 79], Operation::Add(2), 3, 1, 6),
     ];
 
     //
@@ -67,24 +43,47 @@ fn part2(mut monkeys: Vec<Monkey>) -> u64 {
 }
 
 #[derive(Clone)]
-struct Monkey<'a>
-{
+enum Operation {
+    Add(u64),
+    Multiply(u64),
+    Square,
+}
+
+#[derive(Clone)]
+struct Monkey {
     items: VecDeque<u64>,
-    operation: &'a dyn Fn(u64) -> u64,
+    operation: Operation,
     modulo: u64,
     is_divisible: usize,
     not_divisible: usize,
     inspected: u64,
 }
 
-impl<'a> Monkey<'a> {
-    fn new(items: &[u64], operation: &'a dyn Fn(u64) -> u64, modulo: u64, is_divisible: usize, not_divisible: usize) -> Self {
-        Self { items: VecDeque::from_iter(items.iter().copied()), operation, modulo, is_divisible, not_divisible, inspected: 0 }
+impl Monkey {
+    fn new(
+        items: &[u64],
+        operation: Operation,
+        modulo: u64,
+        is_divisible: usize,
+        not_divisible: usize,
+    ) -> Self {
+        Self {
+            items: VecDeque::from_iter(items.iter().copied()),
+            operation,
+            modulo,
+            is_divisible,
+            not_divisible,
+            inspected: 0,
+        }
     }
 
     fn throw(&mut self, relief: bool, common_modulo: u64) -> Option<(u64, usize)> {
         let mut worry = self.items.pop_front()?;
-        worry = (self.operation)(worry);
+        worry = match self.operation {
+            Operation::Add(n) => worry + n,
+            Operation::Multiply(n) => worry * n,
+            Operation::Square => worry * worry,
+        };
         if relief {
             worry /= 3;
         } else {
@@ -115,7 +114,10 @@ impl<'a> Monkey<'a> {
     }
 
     fn monkey_business(monkeys: &[Monkey]) -> u64 {
-        let mut inspections = monkeys.iter().map(|monkey| monkey.inspected).collect::<Vec<_>>();
+        let mut inspections = monkeys
+            .iter()
+            .map(|monkey| monkey.inspected)
+            .collect::<Vec<_>>();
         inspections.sort();
         inspections.reverse();
         inspections[0] * inspections[1]
@@ -125,16 +127,34 @@ impl<'a> Monkey<'a> {
 #[test]
 fn test_rounds_part2() {
     let mut monkeys = vec![
-        Monkey::new(&[79, 98], &|old| old*19, 23, 2, 3),
-        Monkey::new(&[54, 65, 75, 74], &|old| old+6, 19, 2, 0),
-        Monkey::new(&[79, 60, 97], &|old| old*old, 13, 1, 3),
-        Monkey::new(&[74], &|old| old+3, 17, 0, 1),
+        Monkey::new(&[79, 98], Operation::Multiply(19), 23, 2, 3),
+        Monkey::new(&[54, 65, 75, 74], Operation::Add(6), 19, 2, 0),
+        Monkey::new(&[79, 60, 97], Operation::Square, 13, 1, 3),
+        Monkey::new(&[74], Operation::Add(3), 17, 0, 1),
     ];
     Monkey::many_rounds(&mut monkeys, 20, false);
-    assert_eq!(monkeys.iter().map(|monkey| monkey.inspected).collect::<Vec<_>>(), vec![99, 97, 8, 103]);
+    assert_eq!(
+        monkeys
+            .iter()
+            .map(|monkey| monkey.inspected)
+            .collect::<Vec<_>>(),
+        vec![99, 97, 8, 103]
+    );
     Monkey::many_rounds(&mut monkeys, 980, false);
-    assert_eq!(monkeys.iter().map(|monkey| monkey.inspected).collect::<Vec<_>>(), vec![5204, 4792, 199, 5192]);
+    assert_eq!(
+        monkeys
+            .iter()
+            .map(|monkey| monkey.inspected)
+            .collect::<Vec<_>>(),
+        vec![5204, 4792, 199, 5192]
+    );
     Monkey::many_rounds(&mut monkeys, 9000, false);
-    assert_eq!(monkeys.iter().map(|monkey| monkey.inspected).collect::<Vec<_>>(), vec![52166, 47830, 1938, 52013]);
+    assert_eq!(
+        monkeys
+            .iter()
+            .map(|monkey| monkey.inspected)
+            .collect::<Vec<_>>(),
+        vec![52166, 47830, 1938, 52013]
+    );
     assert_eq!(Monkey::monkey_business(&monkeys), 2713310158);
 }
