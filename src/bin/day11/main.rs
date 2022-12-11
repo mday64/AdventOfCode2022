c2adc2129dba1f58d8ed995c788ebc692a1a1a5c
Mark Day <mday@mac.com>
2022-12-11 11:28:35 -0700

    Day 11, part 2 (wrong answer)

--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -6,80 +6,112 @@ fn main() {
     // significantly to the difficulty of this problem.  Instead, I'm
     // going to build them directly in code.
     //
-    let mut monkeys = vec![
+    let monkeys = vec![
         Monkey::new(
             &[57, 58],
             &|old| old*19,
-            &|worry| if worry % 7 == 0 { 2 } else { 3 }),
+            7, 2, 3),
         Monkey::new(
             &[66, 52, 59, 79, 94, 73],
             &|old| old+1,
-            &|worry| if worry % 19 == 0 { 4 } else { 6 }),
+            19, 4, 6),
         Monkey::new(
             &[80],
             &|old| old+6,
-            &|worry| if worry % 5 == 0 { 7 } else { 5 }),
+            5, 7, 5),
         Monkey::new(
             &[82, 81, 68, 66, 71, 83, 75, 97],
             &|old| old+5,
-            &|worry| if worry % 11 == 0 { 5 } else { 2 }),
+            11, 5, 2),
         Monkey::new(
             &[55, 52, 67, 70, 69, 94, 90],
             &|old| old*old,
-            &|worry| if worry % 17 == 0 { 0 } else { 3 }),
+            17, 0, 3),
         Monkey::new(
             &[69, 85, 89, 91],
             &|old| old+7,
-            &|worry| if worry % 13 == 0 { 1 } else { 7 }),
+            13, 1, 7),
         Monkey::new(
             &[75, 53, 73, 52, 75],
             &|old| old*7,
-            &|worry| if worry % 2 == 0 { 0 } else { 4 }),
+            2, 0, 4),
         Monkey::new(
             &[94, 60, 79],
             &|old| old+2,
-            &|worry| if worry % 3 == 0 { 1 } else { 6 }),
+            3, 1, 6),
     ];
 
     //
     // Part 1
     //
+    let result1 = part1(monkeys.clone());
+    println!("Part 1: {}", result1);
+    assert_eq!(result1, 50830);
+
+    //
+    // Part 2
+    //
+    let result2 = part2(monkeys.clone());
+    println!("Part 2: {}", result2);
+    assert_eq!(result2, 14397840080);
+}
+
+fn part1(mut monkeys: Vec<Monkey>) -> u64 {
     for _ in 0..20 {
-        Monkey::one_round(&mut monkeys);
+        Monkey::one_round(&mut monkeys, true);
     }
     let mut inspections = monkeys.iter().map(|monkey| monkey.inspected).collect::<Vec<_>>();
     inspections.sort();
     inspections.reverse();
-    let result1 = inspections[0] * inspections[1];
-    println!("Part 1: {}", result1);
-    assert_eq!(result1, 50830);
+    inspections[0] * inspections[1]
 }
 
+fn part2(mut monkeys: Vec<Monkey>) -> u64 {
+    for _ in 0..10000 {
+        Monkey::one_round(&mut monkeys, false);
+    }
+    let mut inspections = monkeys.iter().map(|monkey| monkey.inspected).collect::<Vec<_>>();
+    inspections.sort();
+    inspections.reverse();
+    inspections[0] * inspections[1]
+}
+
+#[derive(Clone)]
 struct Monkey<'a>
 {
     items: VecDeque<u32>,
     operation: &'a dyn Fn(u32) -> u32,
-    test: &'a dyn Fn(u32) -> usize,
-    inspected: u32,
+    modulo: u32,
+    is_divisible: usize,
+    not_divisible: usize,
+    inspected: u64,
 }
 
 impl<'a> Monkey<'a> {
-    fn new(items: &[u32], operation: &'a dyn Fn(u32) -> u32, test: &'a dyn Fn(u32) -> usize) -> Self {
-        Self { items: VecDeque::from_iter(items.iter().copied()), operation, test, inspected: 0 }
+    fn new(items: &[u32], operation: &'a dyn Fn(u32) -> u32, modulo: u32, is_divisible: usize, not_divisible: usize) -> Self {
+        Self { items: VecDeque::from_iter(items.iter().copied()), operation, modulo, is_divisible, not_divisible, inspected: 0 }
     }
 
-    fn throw(&mut self) -> Option<(u32, usize)> {
+    fn throw(&mut self, relief: bool) -> Option<(u32, usize)> {
         let mut worry = self.items.pop_front()?;
         worry = (self.operation)(worry);
-        worry /= 3;
-        let destination = (self.test)(worry);
+        if relief {
+            worry /= 3;
+        } else {
+            worry %= self.modulo;
+        }
+        let destination = if worry % self.modulo == 0 {
+            self.is_divisible
+        } else {
+            self.not_divisible
+        };
         self.inspected += 1;
         Some((worry, destination))
     }
 
-    fn one_round(monkeys: &mut [Monkey]) {
+    fn one_round(monkeys: &mut [Monkey], relief: bool) {
         for i in 0..monkeys.len() {
-            while let Some((worry, destination)) = monkeys[i].throw() {
+            while let Some((worry, destination)) = monkeys[i].throw(relief) {
                 monkeys[destination].items.push_back(worry);
             }
         }
