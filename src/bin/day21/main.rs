7c8d26a818cb6b89d2ed534b42e75cd6cbdf085b
Mark Day <mday@mac.com>
2022-12-21 13:01:15 -0700

    Don't bother caching monkey results

--- src/bin/day21/main.rs
+++ src/bin/day21/main.rs
@@ -12,18 +12,24 @@ fn main() {
 }
 
 fn part1(input: &str) -> MonkeyNumber {
-    let mut monkeys = parse_input(input);
-    monkey_eval("root", &mut monkeys)
+    let monkeys = parse_input(input);
+    monkey_eval("root", &monkeys)
 }
 
-fn monkey_eval(name: &str, monkeys: &mut HashMap<&str, MonkeyInfo>) -> MonkeyNumber {
-    let info = monkeys.get(name).unwrap();
-    if let Some(result) = info.cached {
-        return result;
-    }
-    drop(info);
-    let result = match info.job {
-        MonkeyJob::Yell(num) => num,
+fn part2(input: &str) -> MonkeyNumber {
+    let _monkeys = parse_input(input);
+
+    // I'm tempted to try changing root's operation to Sub, so that it
+    // returns 0 for equality, and try changing humn's value to see how
+    // root's value changes.  Hopefully, there is a linear relationship,
+    // and I can extrapolate the correct value for humn.
+
+    todo!()
+}
+
+fn monkey_eval(name: &str, monkeys: &HashMap<&str, MonkeyJob>) -> MonkeyNumber {
+    let result = match monkeys.get(name).unwrap() {
+        MonkeyJob::Yell(num) => *num,
         MonkeyJob::Add(left, right) =>
             monkey_eval(left, monkeys) + monkey_eval(right, monkeys),
         MonkeyJob::Sub(left, right) =>
@@ -33,17 +39,16 @@ fn monkey_eval(name: &str, monkeys: &mut HashMap<&str, MonkeyInfo>) -> MonkeyNum
         MonkeyJob::Div(left, right) =>
             monkey_eval(left, monkeys) / monkey_eval(right, monkeys),
     };
-    monkeys.get_mut(name).unwrap().cached = Some(result);
     result
 }
 
-fn parse_input(input: &str) -> HashMap<&str, MonkeyInfo> {
-    let mut monkeys = HashMap::<&str, MonkeyInfo>::new();
+fn parse_input(input: &str) -> HashMap<&str, MonkeyJob> {
+    let mut monkeys = HashMap::<&str, MonkeyJob>::new();
 
     for line in input.lines() {
         let (name, job) = line.split_once(": ").unwrap();
         if let Ok(num) = job.parse::<MonkeyNumber>() {
-            monkeys.insert(name, MonkeyInfo{cached: Some(num), job: MonkeyJob::Yell(num)});
+            monkeys.insert(name, MonkeyJob::Yell(num));
         } else {
             let (left, operator, right) = job.split(' ').collect_tuple().unwrap();
             let job = match operator {
@@ -53,7 +58,7 @@ fn parse_input(input: &str) -> HashMap<&str, MonkeyInfo> {
                 "/" => MonkeyJob::Div(left, right),
                 _ => panic!("Invalid operation: {}", job)
             };
-            monkeys.insert(name, MonkeyInfo{cached: None, job});
+            monkeys.insert(name, job);
         }
     }
 
@@ -68,11 +73,6 @@ enum MonkeyJob<'a> {
     Sub(&'a str, &'a str),
     Mul(&'a str, &'a str),
     Div(&'a str, &'a str),
-}
-
-struct MonkeyInfo<'a> {
-    cached: Option<MonkeyNumber>,
-    job: MonkeyJob<'a>
 }
 
 #[test]
@@ -95,4 +95,26 @@ drzm: hmdt - zczc
 hmdt: 32
 ";
     assert_eq!(part1(input), 152);
+}
+
+#[test]
+fn test_part2() {
+    let input = "\
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+";
+    assert_eq!(part2(input), 301);
 }
