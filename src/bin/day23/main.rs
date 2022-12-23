ef82c0cd817dfd5d2144ae606c2124fb8445ca48
Mark Day <mday@mac.com>
2022-12-23 15:16:25 -0700

    Refactor: add function for one round

--- src/bin/day23/main.rs
+++ src/bin/day23/main.rs
@@ -5,8 +5,9 @@ fn main() {
     let path = std::env::args().skip(1).next()
         .unwrap_or("src/bin/day23/input.txt".into());
     let input = std::fs::read_to_string(path).unwrap();
+    let positions = parse_input(&input);
 
-    let result1 = part1(&input);
+    let result1 = part1(positions);
     println!("Part 1: {result1}");
     assert_eq!(result1, 3882);
 }
@@ -28,99 +29,11 @@ fn main() {
 //      Perhaps just a vec![0,1,2,3] that we iterate over and match
 //      on the value to do the direction-dependent logic.
 //
-fn part1(input: &str) -> usize {
-    // Parse the input to produce the initial state
-    let mut positions = HashSet::<Point>::new();
-    for (y, line) in input.lines().enumerate() {
-        for (x, ch) in line.chars().enumerate() {
-            if ch == '#' {
-                positions.insert(Point{x: x as i32, y: y as i32});
-            }
-        }
-    }
-
+fn part1(mut positions: HashSet<Point>) -> usize {
     use Direction::*;
     let mut directions = vec![North, South, West, East];
     for _ in 0 .. 10 {
-        // Compute the proposed moves and positions
-        let mut proposed_moves = HashMap::<Point, Point>::new();
-        let mut proposed_positions = HashMultiSet::<Point>::new();
-
-        for &Point{x,y} in positions.iter() {
-            if !positions.contains(&Point{x: x-1, y: y-1}) &&
-               !positions.contains(&Point{x,      y: y-1}) &&
-               !positions.contains(&Point{x: x+1, y: y-1}) &&
-               !positions.contains(&Point{x: x-1, y     }) &&
-               !positions.contains(&Point{x: x+1, y     }) &&
-               !positions.contains(&Point{x: x-1, y: y+1}) &&
-               !positions.contains(&Point{x     , y: y+1}) &&
-               !positions.contains(&Point{x: x+1, y: y+1})
-            {
-                // The elf stays where it is
-                proposed_moves.insert(Point{x,y}, Point{x,y});
-                proposed_positions.insert(Point{x,y});
-                continue;
-            }
-
-            let mut new_x = x;
-            let mut new_y = y;
-            for dir in directions.iter().copied() {
-                match dir {
-                    North => {
-                        if !positions.contains(&Point{x: x-1, y: y-1}) &&
-                           !positions.contains(&Point{x, y: y-1}) &&
-                           !positions.contains(&Point{x: x+1, y: y-1})
-                        {
-                            new_y = y - 1;
-                            break;
-                        }
-                    },
-                    South => {
-                        if !positions.contains(&Point{x: x-1, y: y+1}) &&
-                           !positions.contains(&Point{x, y: y+1}) &&
-                           !positions.contains(&Point{x: x+1, y: y+1})
-                        {
-                            new_y = y + 1;
-                            break;
-                        }
-                    },
-                    West => {
-                        if !positions.contains(&Point{x: x-1, y: y-1}) &&
-                           !positions.contains(&Point{x: x-1, y}) &&
-                           !positions.contains(&Point{x: x-1, y: y+1})
-                        {
-                            new_x = x - 1;
-                            break;
-                        }
-                    },
-                    East => {
-                        if !positions.contains(&Point{x: x+1, y: y-1}) &&
-                           !positions.contains(&Point{x: x+1, y}) &&
-                           !positions.contains(&Point{x: x+1, y: y+1})
-                        {
-                            new_x = x + 1;
-                            break;
-                        }
-                    },
-                }
-            }
-            proposed_moves.insert(Point{x,y}, Point{x: new_x, y: new_y});
-            proposed_positions.insert(Point{x: new_x, y: new_y});
-        }
-
-        // Update positions
-        positions.clear();
-        for (old_pos, new_pos) in proposed_moves.into_iter() {
-            if proposed_positions.count_of(&new_pos) == 1 {
-                positions.insert(new_pos);
-            } else {
-                positions.insert(old_pos);
-            }
-        }
-
-        // Rotate the order of directions to consider for next round
-        let dir = directions.remove(0);
-        directions.push(dir);
+        one_round(&mut positions, &mut directions);
     }
 
     // Compute how many empty spaces within the tightest bounding rectangle
@@ -135,6 +48,109 @@ fn part1(input: &str) -> usize {
         max_y = max_y.max(p.y);
     }
     (max_x - min_x + 1) as usize * (max_y - min_y + 1) as usize - positions.len()
+}
+
+// Do one round of movements.
+// Return true if any elf moved.
+fn one_round(positions: &mut HashSet<Point>, directions:&mut Vec<Direction>) -> bool {
+    let mut moved = false;
+
+    // Compute the proposed moves and positions
+    let mut proposed_moves = HashMap::<Point, Point>::new();
+    let mut proposed_positions = HashMultiSet::<Point>::new();
+
+    for &Point{x,y} in positions.iter() {
+        if !positions.contains(&Point{x: x-1, y: y-1}) &&
+            !positions.contains(&Point{x,      y: y-1}) &&
+            !positions.contains(&Point{x: x+1, y: y-1}) &&
+            !positions.contains(&Point{x: x-1, y     }) &&
+            !positions.contains(&Point{x: x+1, y     }) &&
+            !positions.contains(&Point{x: x-1, y: y+1}) &&
+            !positions.contains(&Point{x     , y: y+1}) &&
+            !positions.contains(&Point{x: x+1, y: y+1})
+        {
+            // The elf stays where it is
+            proposed_moves.insert(Point{x,y}, Point{x,y});
+            proposed_positions.insert(Point{x,y});
+            continue;
+        }
+
+        let mut new_x = x;
+        let mut new_y = y;
+        for dir in directions.iter().copied() {
+            match dir {
+                Direction::North => {
+                    if !positions.contains(&Point{x: x-1, y: y-1}) &&
+                        !positions.contains(&Point{x, y: y-1}) &&
+                        !positions.contains(&Point{x: x+1, y: y-1})
+                    {
+                        new_y = y - 1;
+                        break;
+                    }
+                },
+                Direction::South => {
+                    if !positions.contains(&Point{x: x-1, y: y+1}) &&
+                        !positions.contains(&Point{x, y: y+1}) &&
+                        !positions.contains(&Point{x: x+1, y: y+1})
+                    {
+                        new_y = y + 1;
+                        break;
+                    }
+                },
+                Direction::West => {
+                    if !positions.contains(&Point{x: x-1, y: y-1}) &&
+                        !positions.contains(&Point{x: x-1, y}) &&
+                        !positions.contains(&Point{x: x-1, y: y+1})
+                    {
+                        new_x = x - 1;
+                        break;
+                    }
+                },
+                Direction::East => {
+                    if !positions.contains(&Point{x: x+1, y: y-1}) &&
+                        !positions.contains(&Point{x: x+1, y}) &&
+                        !positions.contains(&Point{x: x+1, y: y+1})
+                    {
+                        new_x = x + 1;
+                        break;
+                    }
+                },
+            }
+        }
+        proposed_moves.insert(Point{x,y}, Point{x: new_x, y: new_y});
+        proposed_positions.insert(Point{x: new_x, y: new_y});
+    }
+
+    // Update positions
+    positions.clear();
+    for (old_pos, new_pos) in proposed_moves.into_iter() {
+        if proposed_positions.count_of(&new_pos) == 1 {
+            if old_pos != new_pos {
+                moved = true;
+            }
+            positions.insert(new_pos);
+        } else {
+            positions.insert(old_pos);
+        }
+    }
+
+    // Rotate the order of directions to consider for next round
+    let dir = directions.remove(0);
+    directions.push(dir);
+
+    moved
+}
+
+fn parse_input(input: &str) -> HashSet<Point> {
+    let mut positions = HashSet::<Point>::new();
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            if ch == '#' {
+                positions.insert(Point{x: x as i32, y: y as i32});
+            }
+        }
+    }
+    positions
 }
 
 #[derive(Hash, PartialEq, Eq)]
@@ -162,5 +178,6 @@ fn test_part1() {
 ##.#.##
 .#..#..
 ";
-    assert_eq!(part1(input), 110);
+    let positions = parse_input(input);
+    assert_eq!(part1(positions), 110);
 }
